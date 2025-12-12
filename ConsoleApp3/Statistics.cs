c64981703ad44b66aeff2fcea9c8f06f53fb1184
Azadi <prg0act@gmail.com>
2025-12-12 17:07:22 +0330

    اضافه کردن هر چهار پروژه به ترتیب سوال داده شده.

---
+++ ConsoleApp3/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Statistics
+    {
+        public string AnalyzeNumbers(int a, ref int b, out int c)
+        {
+            c = a + b;
+            b = b * 2;
+            return a.ToString();
+        }
+    }
+}
